4c98642449ebf5a5f6f10984a2feab0fcb706ab0
MinhHieu20 <72988848+MinhHieu20@users.noreply.github.com>
2022-11-08 17:39:18 +0700

    add library, image and update code js

--- js/main.js
+++ js/main.js
@@ -91,7 +91,15 @@ $(document).ready(function(){
           {
             breakpoint: 1366,
             settings: {
-              slidesToShow: 4,
+              slidesToShow: 5,
+              slidesToScroll: 2,
+              infinite: true,
+            }
+          },
+          {
+            breakpoint: 1200,
+            settings: {
+              slidesToShow:4,
               slidesToScroll: 2,
               infinite: true,
             }
@@ -99,7 +107,7 @@ $(document).ready(function(){
           {
             breakpoint: 1024,
             settings: {
-              slidesToShow: 3,
+              slidesToShow:3,
               slidesToScroll: 2,
               infinite: true,
             }
@@ -107,7 +115,7 @@ $(document).ready(function(){
           {
             breakpoint: 992,
             settings: {
-              slidesToShow: 2,
+              slidesToShow: 3,
               slidesToScroll: 2,
               infinite: true,
             }
@@ -140,32 +148,44 @@ $(document).ready(function(){
     });
 
     $(".content-recruit-outside__btn").click(function(event){
-        $(".modal-recruit").addClass("visible");
-      });
-      
-      $(".modal-recruit-container__close").click(function(){
-        $(".modal-recruit").removeClass("visible");
-      });
-      
-      $(".modal-recruit").click(function() {
-        $(".modal-recruit").removeClass("visible");
-      });
-      $(".modal-recruit-container").click(function(event) {
-        event.stopPropagation();
+      $(".modal-recruit").addClass("visible");
     });
+    $(".modal-recruit-container__close").click(function(){
+      $(".modal-recruit").removeClass("visible");
+    });
+    $(".modal-recruit").click(function() {
+      $(".modal-recruit").removeClass("visible");
+    });
+    $(".modal-recruit-container").click(function(event) {
+      event.stopPropagation();
+    });
+
+    $(".content-recruit-outside__top--btn").click(function(event){
+      $(".modal-map").addClass("visible");
+    });
+    $(".js-modal-close").click(function(){
+      $(".modal-map").removeClass("visible");
+    });
+    $(".modal-map").click(function() {
+      $(".modal-map").removeClass("visible");
+    });
+    $(".modal-maps-address").click(function(event) {
+      event.stopPropagation();
+    });
+
     $('.js-moblie-navbar').click(function() { 
       $('.js-mobile-menu').toggleClass('show'); 
       $('.mobile-menu-overlay').toggleClass('show');  
-      if($('.js-mobile-menu').hasClass('show') === true) {
+      if($('.js-mobile-menu').hasClass('show') === false) {
         $('.navbar-mobile').css("color", "#000")
       } else { 
         $('.navbar-mobile').css("color", "#fff")
       }
     })
     $(".mobile-menu-overlay").click(function() {
-      $('.js-mobile-menu').addClass('show'); 
+      $('.js-mobile-menu').removeClass('show'); 
       $('.mobile-menu-overlay').toggleClass('show');
-      if($('.js-mobile-menu').hasClass('show') === true) {
+      if($('.js-mobile-menu').hasClass('show') === false) {
         $('.navbar-mobile').css("color", "#000")
       } else { 
         $('.navbar-mobile').css("color", "#fff")
@@ -178,7 +198,7 @@ $(document).ready(function(){
       prevArrow: true,
       responsive: [
         {
-          breakpoint: 769,
+          breakpoint: 1024,
           settings: {
             slidesToShow: 3,
             autoplaySpeed: 3000,
@@ -186,7 +206,7 @@ $(document).ready(function(){
           }
         },
         {
-          breakpoint: 481,
+          breakpoint: 740,
           settings: {
             slidesToShow: 1,
             autoplaySpeed: 3000,
@@ -221,7 +241,11 @@ $(document).ready(function(){
       prevArrow: true,
       responsive: [
         {
-          breakpoint: 481,
+          breakpoint: 9999,
+          settings: "unslick"
+        },
+        {
+          breakpoint: 740,
           settings: {
             slidesToShow: 1,
             autoplaySpeed: 3000,
@@ -283,5 +307,42 @@ $(document).ready(function(){
           }
       });
     });
+    $("a[href^='#']").click(function(e) {
+      e.preventDefault();
+      
+      var position = $($(this).attr("href")).offset().top;
+    
+      $("body, html").animate({
+        scrollTop: position
+      } /* speed */ );
+    });
 })
+var sliderBool = false,
+  sliderBreakpoint = 739,
+  sliderSettings = {
+  mobileFirst: true,
+  rows:2,
+  dots: true,
+  autoplay:true,
+	responsive: [
+		{
+      breakpoint: sliderBreakpoint,
+      settings: "unslick"
+		}
+	]
+};
+function sliderInit(sliderElem) {
+	if (window.innerWidth <= sliderBreakpoint) {
+    if (sliderBool == false) {
+      $("" + sliderElem + "").slick(sliderSettings);
+      sliderBool = true;
+    }
+	} else {
+		sliderBool = false;
+	}
+}
+sliderInit(".career-box");
+$(window).resize(function () {
+	sliderInit(".career-box");
+});
 AOS.init();
